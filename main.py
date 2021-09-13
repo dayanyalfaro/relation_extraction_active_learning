968c164630776f48659fbf6c30ab8cbe2fad9b07
dayanyalfaro <dayanyalfaro@gmail.com>
2021-09-12 23:21:17 -0400

    remove AL cycle

--- main.py
+++ main.py
@@ -61,7 +61,8 @@ def main(cfg):
 
     all_train_ds = load_pkl(train_data_path)
     random.shuffle(all_train_ds)
-    cur_labeled_ds = all_train_ds[:cfg.start_size]
+    cur_labeled_ds = all_train_ds
+    # cur_labeled_ds = all_train_ds[:cfg.start_size]
     unlabeled_ds = all_train_ds[cfg.start_size:]
 
     per_log_num = 400
@@ -75,8 +76,8 @@ def main(cfg):
     test_f1_scores, test_losses = [], []
     while len(cur_labeled_ds) <= all_size:
         model = __Model__[cfg.model.model_name](cfg)
-        if len(cur_labeled_ds) == cfg.start_size:
-            logger.info(f'\n {model}')
+        # if len(cur_labeled_ds) == cfg.start_size: #TODO
+        logger.info(f'\n {model}')
         model.to(device)
         optimizer = optim.Adam(model.parameters(), lr=cfg.learning_rate, weight_decay=cfg.weight_decay)
         scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=cfg.lr_factor, patience=cfg.lr_patience)
@@ -100,16 +101,16 @@ def main(cfg):
             valid_losses.append(valid_loss)
             one_f1_scores.append(valid_f1)
 
-        if cfg.show_plot and cfg.plot_utils == 'tensorboard' and (len(cur_labeled_ds) - cfg.start_size) % per_log_num == 0:
-            logger.info(f'one_f1_scores:{one_f1_scores}')
-            for i in range(len(train_losses)):
-                writer.add_scalars(f'valid_copy/valid_loss_{len(cur_labeled_ds)}', {
-                    'train': train_losses[i],
-                    'valid': valid_losses[i]
-                }, i)
-                writer.add_scalars(f'valid/valid_f1_score_{len(cur_labeled_ds)}', {
-                    'valid_f1_score': one_f1_scores[i]
-                }, i)
+        # if cfg.show_plot and cfg.plot_utils == 'tensorboard' and (len(cur_labeled_ds) - cfg.start_size) % per_log_num == 0:
+        logger.info(f'one_f1_scores:{one_f1_scores}') #TODO
+        for i in range(len(train_losses)):
+            writer.add_scalars(f'valid_copy/valid_loss_{len(cur_labeled_ds)}', {
+                'train': train_losses[i],
+                'valid': valid_losses[i]
+            }, i)
+            writer.add_scalars(f'valid/valid_f1_score_{len(cur_labeled_ds)}', {
+                'valid_f1_score': one_f1_scores[i]
+            }, i)
 
         test_f1, test_loss = validate(-1, model, test_dataloader, criterion, device, cfg)
         test_f1_scores.append(test_f1)
