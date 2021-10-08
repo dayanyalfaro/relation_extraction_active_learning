ab173fbbaf6718051ea0783b5c7ebb872c64d671
dayanyalfaro <dayanyalfaro@gmail.com>
2021-10-08 02:09:41 -0400

    add uncertainty query strategies

--- data_select.py
+++ data_select.py
@@ -133,9 +133,9 @@ class QueryUncertainty(QueryBase):
         self.type = cfg.strategy.type
         self.cfg = cfg
 
-    def get_probs_model(self, model, unlabeled_ds):
+    def predict_prob (self, model, unlabeled_ds):
         model.eval()
-        all_y_pred = np.empty((0, self.num_relations))
+        all_y_pred = torch.empty((0, self.num_relations))
         for index, one in enumerate(unlabeled_ds):
             one_dataloader = DataLoader([one], collate_fn=collate_fn(self.cfg))
             (x, y) = next(iter(one_dataloader))
@@ -143,42 +143,34 @@ class QueryUncertainty(QueryBase):
                 x[key] = value.to(self.device)
             with torch.no_grad():
                 y_pred = model(x)
-            y_pred = y_pred.cpu().detach().numpy()
-            all_y_pred = np.concatenate((all_y_pred, y_pred), axis=0)
+            y_pred = y_pred.cpu().detach()
+            all_y_pred = torch.cat((all_y_pred, y_pred), 0)
 
-        all_y_pred = torch.from_numpy(all_y_pred)
-        all_y_pred_probability = nn.functional.softmax(all_y_pred, dim=1).numpy()
+        all_y_pred_probability = nn.functional.softmax(all_y_pred, dim=1)
         return all_y_pred_probability
 
     def pre_sample(self,cur_labeled_ds, unlabeled_ds, model):
-        all_y_pred_probability = self.get_probs_model(model, unlabeled_ds)
+        probs = self.predict_prob(model, unlabeled_ds)
         select = None
+        values = None
+        U = None
+        idxs_unlabeled = np.array(list(unlabeled_ds.keys()))
+
         if self.type == 'least_confident':
-            # The most likely label probability predicted by the current model is the smallest
-            tmp = all_y_pred_probability.max(axis=1)
-            select = heapq.nsmallest(self.batch_size, range(len(tmp)), tmp.take)
+            U = probs.max(1)[0]
         elif self.type == 'margin_sampling':
-            res = np.empty(0)
-            ttmp = np.vsplit(all_y_pred_probability, all_y_pred_probability.shape[0])
-            for tmp in ttmp:
-                tmp = np.squeeze(tmp)
-                # Take the two largest numbers
-                first_two = tmp[np.argpartition(tmp, -2)[-2:]]
-                # Collect the difference between the largest two numbers
-                res = np.concatenate((res, np.array([abs(first_two[0] - first_two[1])])), axis=0)
-            select = heapq.nsmallest(self.batch_size, range(len(res)), res.take)
+            probs_sorted, idxs = probs.sort(descending=True)
+            U = probs_sorted[:, 0] - probs_sorted[:,1]
         elif self.type == 'entropy_sampling':
-            res = np.empty(0)
-            ttmp = np.vsplit(all_y_pred_probability, all_y_pred_probability.shape[0])
-            for tmp in ttmp:
-                tmp = np.squeeze(tmp)
-                # .The dot method seems to be able to automatically transpose and then find the dot product
-                res = np.concatenate((res, np.array([tmp.dot(np.log2(tmp))])), axis=0)
-            select = heapq.nsmallest(self.batch_size, range(len(res)), res.take)
+            log_probs = torch.log(probs)
+            U = (probs*log_probs).sum(1)
         else:
             assert ('uncertainty concrete choose error')
 
-        return select
+        sorted, idxs = U.sort()
+        select = idxs_unlabeled[idxs[:self.pre_batch_size]]
+        values = sorted[:self.pre_batch_size]
+        return select, values
 
 # class QueryUncertainty(QueryBase):
 #     def __init__(self, cfg, device):
