84481ace3268ae761a4297f267280f5a2198828b
dayanyalfaro <dayanyalfaro@gmail.com>
2021-11-16 00:33:55 -0500

    remove random

--- experiment1.sh
+++ experiment1.sh
@@ -1,4 +1,3 @@
 python main.py 'balance=False' 'strategy=bald' 
 python main.py 'balance=True' 'strategy=bald'
 python main.py 'active_learning=False'
-python main.py 'balance=False' 'strategy=random'
